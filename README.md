dfa5618cc61a2b4cc924cd2684dcf1f8e08cb3a3
abuccts <abuccts@gmail.com>
2015-12-21 21:25:13 +0800

    update frame

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # redesigned-umbrella
+
+## build
+```
+git clone git@github.com:Abuccts/redesigned-umbrella.git
+cd redesigned-umbrella
+npm install
+```
+
+## run
+```
+node app.js
+```
---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var fs = require('fs');
+var process = require('child_process');
+var multipart = require('connect-multiparty');
+
+var multipartMiddleware = multipart();
+var app = express();
+
+app.use(express.static('public'));
+
+// ROUTES
+app.get('/', function(req, res) {
+  res.sendFile('public/index.html', {root: './'});
+});
+
+app.post('/public', multipartMiddleware, function(req, res) {
+	var seq = req.body.seq;
+	var sh = 'echo';
+	console.log('query: ' + seq);
+	process.execFile(sh, [seq], null, function(error, stdout, stderr) {
+		if (error !== null) {
+			console.log('exec error: ' + error);
+			res.json({
+				status: -1,
+				})
+		}
+		else {
+			console.log(stdout);
+			res.json({
+				status: 0,
+				data: stdout
+			});
+		}
+	});
+});
+
+var server = app.listen(8008, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Server listening at http://%s:%s', host, port);
+});
