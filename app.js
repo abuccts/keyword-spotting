d914456246ec97a5df27a9283fbb5d0e8aea05aa
abuccts <abuccts@gmail.com>
2015-12-24 10:31:24 +0800

    fix post bug

--- app.js
+++ app.js
@@ -15,25 +15,23 @@ app.get('/', function(req, res) {
 
 app.post('/public', multipartMiddleware, function(req, res) {
 	var seq = req.body.seq.replace(/ /g, '_');
-	var sh = 'echo';
+	var sh = './test.sh';
 	console.log('query: ' + seq);
 	console.log('shell: ' + sh);
 	process.execFile(sh, [seq], null, function(error, stdout, stderr) {
 		if (error !== null) {
 			console.log('exec error: ' + error);
 			res.json({
-				status: -1,
+				"status": -1
 			});
 		}
 		else {
 			console.log(stdout);
 			//console.log(stderr);
-			//var ans = [['20140202320', '发射点反对犯得上反对俗话'], ['dasdsaf238193209B', '多久啊还丰富的省份'], ['23213213A', '蒋介石的恢复']];
 			res.json({
-				status: 0,
-				data: stdout
+				"status": 0,
+				"data": stdout
 			});
-			console.log(res);
 		}
 	});
 });
