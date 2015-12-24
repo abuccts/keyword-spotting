1f6fa8eec388b3e60b3189deb0b6a8b96d128061
abuccts <abuccts@gmail.com>
2015-12-24 13:17:36 +0800

    update load gif

---
+++ public/js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $('button').click(function() {
+	$('#result').html('<img src="img/load.gif">');
+	var query = new FormData();
+	query.append('seq', document.getElementById('input').value);
+	document.getElementById('input').value = '';
+	$.ajax({
+			type: 'POST',
+			url: '/public',
+			data: query,
+			processData: false,
+			contentType: false,
+			dataType: 'JSON',
+			success: function(res) {
+				if (res.status == 0) {
+					$('#result').html('<table class="table"><tbody></tbody></table>');
+					$.each(res.data, function(i, field) {
+						$('#result').children('table').children('tbody').append('<tr><td>' + 
+									i + '</td><td>' + field[0] + '</td><td>' + field[1] + '</td></tr>');
+					});
+					//console.log(res.data);
+				}
+				else {
+					$('#result').html('<h2>Not Found : (</h2>');
+				}
+				document.getElementById('input').focus();
+			},
+			error: function(res) {
+				console.log(arguments);
+			}
+		});
+	});
+});
